f535aafc3ef565319b343c6b05f11fc51b85f8e6
Joao Victor <joaovictorcorrealuzia5@proton.me>
2026-09-22 17:04:12 -0300

    Iniciado o projeto

---
+++ src/main/java/com/br/joaovictor/desafiopagnet/DesafioPagnetApplication.java
@@ -0,0 +1,13 @@
+package com.br.joaovictor.desafiopagnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPagnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPagnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/joaovictor/desafiopagnet/DesafioPagnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.joaovictor.desafiopagnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPagnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
